b4f37ad2addcc63ed59169e3674241a3b176f37a
elisadimiti <elisadimiti5@gmail.com>
2022-03-02 17:07:59 +0300

    Updated file

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,5 @@
-
 #!/usr/bin/node
-// Script that prints 3 lines: (like 1-multi_languages.js) but by
-// using an array of string and a loop
+// Script that prints 3 lines
 
 const langs = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 
